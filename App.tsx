75d732d4fd08f0b18a143d01eb4768f288c2b61e
tinh phan <21522683@gm.uit.edu.vn>
2024-04-07 14:54:35 +0700

    .

--- App.tsx
+++ App.tsx
@@ -1,17 +1,30 @@
-import {View, Text, SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
-import HomeScreen from './src/screens/HomeScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-const Stack = createNativeStackNavigator();
-export default function App() {
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import React from 'react';
+// import HomeScreen from './src/screens/HomeScreen';
+// import ProfileScreen from './src/screens/ProfileScreen';
+// const Stack = createNativeStackNavigator();
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Profile" component={ProfileScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './src/screens/HomeScreen/style'
+
+const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <View>
+      <Text style={{fontSize: 40, color: '#000'}}>App nha</Text>
+    </View>
+  )
 }
+
+export default App
