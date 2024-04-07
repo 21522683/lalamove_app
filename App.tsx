201c6b0a51081258fd68a5e6bf7a6516684bd64e
tinh phan <21522683@gm.uit.edu.vn>
2024-04-07 14:56:56 +0700

    .

--- App.tsx
+++ App.tsx
@@ -1,30 +1,16 @@
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import React from 'react';
-// import HomeScreen from './src/screens/HomeScreen';
-// import ProfileScreen from './src/screens/ProfileScreen';
-// const Stack = createNativeStackNavigator();
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Home">
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//         <Stack.Screen name="Profile" component={ProfileScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from './src/screens/HomeScreen/style'
-
-const App = () => {
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+const Stack = createNativeStackNavigator();
+export default function App() {
   return (
-    <View>
-      <Text style={{fontSize: 40, color: '#000'}}>App nha</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default App
