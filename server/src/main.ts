054d7f66bbbd8aae05f968d3f7c969de37f6f93b
Fatitboo <21522448@gm.uit.edu.vn>
2024-04-24 23:19:14 +0700

    source back end in lalamove forlder

---
+++ server/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+//Alj8zjz8KqlaCoqb
+// 21522448
+//mongodb+srv://21522448:Alj8zjz8KqlaCoqb@lalamove.yy2l8yg.mongodb.net/?retryWrites=true&w=majority&appName=lalamove
+//mongodb+srv://21522448:Alj8zjz8KqlaCoqb@lalamove.yy2l8yg.mongodb.net/
+bootstrap();
