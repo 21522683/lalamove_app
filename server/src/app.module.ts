936915d5d815a6ccd35222d578d4189e0dd0649a
Fatitboo <21522448@gm.uit.edu.vn>
2024-04-25 12:01:18 +0700

    set up nhe  nhe

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import {MongooseModule} from '@nestjs/mongoose'
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://21522448:Alj8zjz8KqlaCoqb@lalamove.yy2l8yg.mongodb.net/?retryWrites=true&w=majority&appName=lalamove'), UserModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://21522448:Alj8zjz8KqlaCoqb@lalamove.yy2l8yg.mongodb.net/?retryWrites=true&w=majority&appName=lalamove',
+    ),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
